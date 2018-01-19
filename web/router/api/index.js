a35dfcd6249b1d43ae1bd6e6c7a0edc441281ea0
ajalabert <jalabert.ad@gmail.com>
2018-01-19 22:27:55 +0100

    add products (get one, get all)

--- web/router/api/index.js
+++ web/router/api/index.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 
 router.use('/user', require('./user/index.js'));
+router.use('/products', require('./product/index.js'));
 //router.use('/admin', require('/admin/index.js'));
 
 module.exports = router;
